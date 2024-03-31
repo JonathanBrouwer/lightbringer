1222f3de07ee9f3ee4f2a684c19e515537a06c74
jonathan <jonathantbrouwer@gmail.com>
2024-03-31 17:02:35 +0200

    Timers working

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@
 #![feature(type_alias_impl_trait)]
 
 use embassy_executor::Spawner;
-// use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
     embassy::{self},
     peripherals::Peripherals,
     prelude::*,
-    timer::TimerGroup,
 };
+use esp_hal::systimer::SystemTimer;
 
 #[embassy_executor::task]
 async fn run() {
     loop {
         esp_println::println!("Hello world from embassy using esp-hal-async!");
-        // Timer::after(Duration::from_millis(1_000)).await;
+        Timer::after(Duration::from_millis(1_000)).await;
     }
 }
 
@@ -28,13 +28,13 @@ async fn main(spawner: Spawner) {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    embassy::init(&clocks, timg0);
+    let timer = SystemTimer::new(peripherals.SYSTIMER);
+    embassy::init(&clocks, timer);
 
     spawner.spawn(run()).ok();
 
     loop {
         esp_println::println!("Bing!");
-        // Timer::after(Duration::from_millis(5_000)).await;
+        Timer::after(Duration::from_millis(5_000)).await;
     }
 }
