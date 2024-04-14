da36997f15a5fef3f9d2e29d5c806e604a010ebf
jonathan <jonathantbrouwer@gmail.com>
2024-04-14 10:22:27 +0200

    Cleanup

--- src/ota/crc.rs
+++ src/ota/crc.rs
@@ -3,12 +3,7 @@ use crc::{Algorithm, Crc};
 /// ESP32 CRC32 implementation (`esp_rom_crc32_le`)
 /// This has only been verified to be identical with one input-output pair so use with caution.
 pub(crate) fn esp_crc32(bytes: &[u8; 4]) -> u32 {
-    // TODO: can this be a one-liner?
-    let mut buffer = [0; 4];
-    for (i, byte) in bytes.iter().enumerate() {
-        buffer[i] = !*byte;
-    }
-
+    let buffer = bytes.map(|v| !v);
     !Crc::<u32>::new(&CRC_32_ESP).checksum(&buffer)
 }
 
